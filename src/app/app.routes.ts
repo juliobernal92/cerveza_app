285786c393940819e1b8a36fff7df254fc3ea94b
Julio Bernal <julio.bernal92@gmail.com>
2025-10-20 19:39:18 -0300

    Anhadir menu tabs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,28 +2,31 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '/tabs/inicio',
     pathMatch: 'full',
   },
   {
-    path: 'inicio',
-    loadComponent: () => import('./pages/inicio/inicio.page').then( m => m.InicioPage)
-  },
-  {
-    path: 'carrito',
-    loadComponent: () => import('./pages/carrito/carrito.page').then( m => m.CarritoPage)
-  },
-  {
-    path: 'perfil',
-    loadComponent: () => import('./pages/perfil/perfil.page').then( m => m.PerfilPage)
-  },
-  {
     path: 'tabs',
-    loadComponent: () => import('./tabs/tabs.page').then( m => m.TabsPage)
+    loadComponent: () => import('./tabs/tabs.page').then(m => m.TabsPage),
+    children: [
+      {
+        path: 'inicio',
+        loadComponent: () => import('./pages/inicio/inicio.page').then(m => m.InicioPage),
+      },
+      {
+        path: 'carrito',
+        loadComponent: () => import('./pages/carrito/carrito.page').then(m => m.CarritoPage),
+      },
+      {
+        path: 'perfil',
+        loadComponent: () => import('./pages/perfil/perfil.page').then(m => m.PerfilPage),
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/inicio',
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
